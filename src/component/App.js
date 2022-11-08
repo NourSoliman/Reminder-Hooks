77eb4848cb293232a5365013899aa5eabcada634
NourSoliman <noursoliman182@yahoo.com>
2022-11-08 23:59:58 +0200

    fixed remove all tasks button

--- src/component/App.js
+++ src/component/App.js
@@ -1,50 +1,69 @@
 import React, { useState } from 'react'
-import { addReminder , removeReminder ,removeAll } from '../actions'
+import { addReminder, removeReminder, removeAll } from '../actions'
 import { connect } from 'react-redux'
 import moment from 'moment'
 
 const App = (props) => {
     const [text, setText] = useState(``);
-    const [desc , setDesc] = useState(``);
-    const [date, setDate] = useState(new Date());
-    let render_Reminder = () => {
+    const [desc, setDesc] = useState(``);
+    const [date, setDate] = useState(``);
+    const render_Reminder = () => {
         const { reminders } = props;
         const length = reminders.length
         console.log(`from reminders `, reminders);
         return (
             <ul>
-            {
-                (length ? reminders.map(reminder => {
-                    return (
-                    <li key={reminder.id}>
-                        <div>{reminder.text}</div>
-                        <div>{reminder.desc}</div>
-                        <div>{moment(new Date(reminder.date)).fromNow()}</div>
-                        <div className='btn btn-danger' onClick={()=>props.removeReminder(reminder.id)} > X</div>
-                    </li>
+                {
+                    (length ? reminders.map(reminder => {
+                        return (
+                            <li key={reminder.id}>
+                                <div>{reminder.text}</div>
+                                <div>{reminder.desc}</div>
+                                <div>{moment(new Date(reminder.date)).fromNow()}</div>
+                                {/* <div>{reminder.date}</div> */}
+                                <div className='btn btn-danger' onClick={() => props.removeReminder(reminder.id)} > X</div>
+                            </li>
                         )
-                }): <h2>There's no tasks to show!</h2> )
-            
-            }
+                    }) : <h2>There's no tasks to show!</h2>)
+                }
             </ul>
         )
-        }
+    }
     console.log(props);
-    const handleSubmit = () => {
-    props.addReminder(text , desc , date);
-    setText(``)
-    setDesc(``)
-    setDate(``)
+    const handleClick = () => {
+        props.addReminder(text, desc, date)
+        setText(``)
+        setDesc(``)
+        setDate(``)
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder='Enter ur task name...' className='form-control' onChange={(e) => setText(e.target.value )}></input>
-            <input type="text" placeholder='Please write a description of your task.... ' className='form-control' onChange={(e)=> setDesc(e.target.value)}></input>
-            <input type="datetime-local" className='form-control' onChange={(e) => setDate(e.target.value)}  ></input>
-            <button  type="Submit" className='btn btn-primary'>Add A task</button>
+        <div>
+            <input type="text"
+                value={text}
+                placeholder='Enter ur task name...'
+                className='form-control'
+                onChange={(e) => setText(e.target.value)}>
+            </input>
+            <input type="text"
+                value={desc}
+                placeholder='Please write a description of your task.... '
+                className='form-control'
+                onChange={(e) => setDesc(e.target.value)}>
+            </input>
+            <input
+                value={date}
+                type="datetime-local"
+                className='form-control'
+                onChange={(e) => setDate(e.target.value)}
+            ></input>
+            <button className='btn btn-primary'
+                onClick={handleClick}>
+                Add A task</button>
             {render_Reminder()}
-            <button className='btn btn-danger' onClick={()=> props.removeAll()}>Remove all Tasks</button>
-        </form>
+            <button className='btn btn-danger'
+                onClick={() => props.removeAll()}>
+                Remove all Tasks</button>
+        </div>
     )
 
 }
@@ -59,4 +78,4 @@ const App = (props) => {
 export default connect(state => {
     return { reminders: state }
 
-}, { addReminder , removeReminder , removeAll  })(App)
+}, { addReminder, removeReminder, removeAll })(App)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { ADD_REMINDER , REMOVE_REMINDER , REMOVE_ALL } from "../actionTypes/type"
 import {bake_cookie , read_cookie} from 'sfcookies'
     let reducer = (state=[], action) => {
-    let reducer = null;
+    let reducer = [];
     state = read_cookie(`reducer`)
     if (action.type === ADD_REMINDER) {
-        reducer = [...state,{text: action.text , desc: action.desc, date: action.date , id: Math.random()}]    
+        reducer = [...state,{text: action.text , desc: action.desc, date: action.date , id:Math.random()}]    
         console.log(`from reducer` , reducer);
         bake_cookie(`reducer`,reducer)
         return reducer
